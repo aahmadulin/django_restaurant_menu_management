68403adac50bde6019f535434525b74f36a20158
Artem Akhmadulin <147542825+aahmadulin@users.noreply.github.com>
2025-02-12 00:18:02 +0300

    urls.py

--- subfolderutf/foods/urls.py
+++ subfolderutf/foods/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import FoodListView
 
 urlpatterns = [
-    path('foods/', FoodListView.as_view(), name='food-list'),  # Обратите внимание на слэш в конце
+    path('foods/', FoodListView.as_view(), name='food-list'), 
 ]
